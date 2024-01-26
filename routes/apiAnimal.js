35e0f6968ddc703d3899c3d624b330f95b0700b5
Nathan00725 <lmnathan25@gmail.com>
2024-01-26 15:57:45 -0600

    Modulos terminado, listo para el Lavoratorio

--- routes/apiAnimal.js
+++ routes/apiAnimal.js
@@ -1,6 +1,11 @@
 import  express  from "express";
 const animal = express();
-import {db} from '../db/conn.js';
+import { getAnimal,
+         postAnimal,
+         putAnimal,
+         deleteAnimal
+         } from "../controller/animalController.js";
+
 
 const animales = [{
     nombre: "perro",
@@ -12,67 +17,15 @@ const animales = [{
     sonido: "miau"
 }]
 
-animal.get('', async (req, res)=>{
-  
-    const sql= `select * from tbl_animal order by id`;
-    const result = await db.query(sql);
-    res.json(result)
-    
-
-})
+animal.get('', getAnimal)
 
 
-animal.post('', async(req, res)=>{
- 
-   //Sintaxis de destructuracion
-   const { nombre , sonido} = req.body;
-
-    const params =  [nombre, sonido];
-
-    const sql = `insert into tbl_animal 
-                (nombre, sonido )
-                values 
-                ($1, $2) returning * `
-
-    const result = await db.query(sql , params);
-
-    res.json(result);
-})
+animal.post('', postAnimal)
 
 
-animal.put('/:id', async (req, res)=>{
-
-    const {nombre, sonido} = req.body
-    const {id} = req.params
-
-    const params = [
-        nombre,
-        sonido,
-        id
-    ]
-    
-    const sql = `update tbl_animal
-                  set
-                  nombre = $1,
-                  sonido = $2
-                where id = $3 returning *`;
- 
-    const result = await db.query(sql, params)
- 
-    res.json(result);
-
-})
+animal.put('/:id', putAnimal)
 
 
-animal.delete('/:id', async (req, res)=>{
-
-    const params = [req.params.id];
-
-    const sql = `delete from tbl_animal where id = $1 returning *`;
-
-    const result = await db.query(sql, params);
-
-    res.json(result);
-})
+animal.delete('/:id', deleteAnimal)
 
 export { animal }
